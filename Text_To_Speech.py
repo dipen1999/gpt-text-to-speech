dd5505bdd9c0a1c97fa0bd9e7c17dc1bb9c36745
louiecerv <lfcervantes@wvsu.edu.ph>
2024-05-01 11:19:15 +0800

    save

--- Text_To_Speech.py
+++ Text_To_Speech.py
@@ -1,9 +1,13 @@
 import streamlit as st
+import os
 
 from pathlib import Path
 from openai import OpenAI
-client = OpenAI()
 
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.getenv("API_key"),
+)
 def app():
     st.title("Text To Speech")
     st.write("This app converts text to speech using OpenAI's API.")
@@ -20,4 +24,3 @@ def app():
 
 if __name__ == "__main__":  
     app()   
-    
